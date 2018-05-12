f062d6a0be7d7c8d9d66bf8fb074087fcc8bb805
Eliot Swank <eliotswank@gmail.com>
2018-05-12 16:17:18 -0600

    builds test for delete function

---
+++ spec/features/jobs/user_deletes_a_job_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "user deletes a job" do
+  describe "they link from the show page" do
+    it "displays all jobs without the deleted entry" do
+      company = Company.create!(name: "ESPN")
+      job_1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+      job_2 = company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City")
+
+      visit company_path(job_1)
+      click_link "Delete"
+
+      expect(current_path).to eq(companies_path)
+      expect(page).to have_content(job_2.title)
+      expect(page).to_not have_content(job_1.title) 
+    end
+  end
+end
