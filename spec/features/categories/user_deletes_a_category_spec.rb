fccaa64b9e5a6802022453bc54db316552c787dd
Eliot Swank <eliotswank@gmail.com>
2018-05-13 17:10:15 -0600

    adds test for delete categories

---
+++ spec/features/categories/user_deletes_a_category_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "user wants to delete category" do
+  describe "user clicks on delete" do
+    it "deletes the category" do
+      category = Category.create(title: 'Foo')
+
+      visit categories_path
+
+      click_link "Delete"
+
+      expect(page).to_not have_content(category)
+    end
+    
+  end
+  
+end
