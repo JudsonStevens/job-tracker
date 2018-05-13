bfcbf506be02049a78f4492a7fd6b7acf272951e
Eliot Swank <eliotswank@gmail.com>
2018-05-12 19:20:10 -0600

    Finishes implementation of delete job functionality. All tests green.

--- spec/features/jobs/user_deletes_a_job_spec.rb
+++ spec/features/jobs/user_deletes_a_job_spec.rb
@@ -4,14 +4,14 @@ describe "user deletes a job" do
   describe "they link from the show page" do
     it "displays all jobs without the deleted entry" do
       company = Company.create!(name: "ESPN")
-      job_1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-      job_2 = company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City")
+      job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
 
-      visit company_job_path(company, job_1)
+      visit company_job_path(company, job)
+
       click_link "Delete"
 
-      expect(page).to have_content(job_2.title)
-      expect(page).to_not have_content(job_1.title)
+      expect(current_path).to eq(company_jobs_path(company))
+      expect(page).to have_content("#{job.title} was successfully deleted")
     end
   end
 end
