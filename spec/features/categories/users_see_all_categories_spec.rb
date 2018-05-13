e205bb1953a779c66712ae385a62e428e4cac2e9
Eliot Swank <eliotswank@gmail.com>
2018-05-12 22:34:02 -0600

    adds test for editing category

--- spec/features/categories/users_see_all_categories_spec.rb
+++ spec/features/categories/users_see_all_categories_spec.rb
@@ -22,5 +22,22 @@ describe "user visits categories index" do
 
     click_link "Foo"
 
-    expect(current_path).to eq(jobs_path) 
+    expect(current_path).to eq(jobs_path)
+  end
+
+  scenario "a user can edit a category" do
+    category = Category.new(title: "Foo")
+
+    visit categories_path
+
+    click_button "Edit"
+
+    fill_in "category[title]",	with: "Foobarbaz"
+
+    click_button "Submit"
+
+    expect(current_path).to eq(categories_path)
+    expect(page).to have_content("Foobarbaz")
+    expect(page).to_not have_content(category)
+  end
 end
