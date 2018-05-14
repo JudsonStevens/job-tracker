bd05f66f3677e0f55edc79b82efb5f091697df27
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-13 20:23:36 -0600

    Implementation of company contact model, views, and tests

---
+++ spec/models/company_contact_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe CompanyContact do
+  describe 'Validations' do
+    describe 'valid attributes' do
+      it 'is valid if created with a name, position, and email' do
+        company = Company.create!(name: 'ESPN')
+        company_contact = company.contacts.create!(name: 'Julie Andrews', position: 'Hiring Manager', email: 'Julie@hr.org')
+
+        expect(company_contact).to be_valid
+      end
+    end
+
+    describe 'invalid attributes' do
+      it 'is invalid if created without a name' do
+        company = Company.create!(name: 'ESPN')
+        company_contact = company.contacts.create!(position: 'Hiring Manager', email: 'Julie@hr.org')
+
+        expect(company_contact).to be_invalid
+      end
+
+      it 'is invalid if created without a position' do
+        company = Company.create!(name: 'ESPN')
+        company_contact = company.contacts.create!(name: 'Julie Andrews', email: 'Julie@hr.org')
+
+        expect(company_contact).to be_invalid
+      end
+
+      it 'is invalid if created without an email' do
+        company = Company.create!(name: 'ESPN')
+        company_contact = company.contacts.create!(name: 'Julie Andrews', position: 'Hiring Manager')
+
+        expect(company_contact).to be_invalid
+      end
+    end
+  end
+end
