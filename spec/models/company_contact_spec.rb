8926105ce8e381d84a6b00eb334bd8f0c368fb71
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-13 20:34:34 -0600

    Implementation of company contact resource and final validation tests

--- spec/models/company_contact_spec.rb
+++ spec/models/company_contact_spec.rb
@@ -5,7 +5,7 @@ describe CompanyContact do
     describe 'valid attributes' do
       it 'is valid if created with a name, position, and email' do
         company = Company.create!(name: 'ESPN')
-        company_contact = company.contacts.create!(name: 'Julie Andrews', position: 'Hiring Manager', email: 'Julie@hr.org')
+        company_contact = company.company_contacts.new(name: 'Julie Andrews', position: 'Hiring Manager', email: 'Julie@hr.org')
 
         expect(company_contact).to be_valid
       end
@@ -14,21 +14,21 @@ describe CompanyContact do
     describe 'invalid attributes' do
       it 'is invalid if created without a name' do
         company = Company.create!(name: 'ESPN')
-        company_contact = company.contacts.create!(position: 'Hiring Manager', email: 'Julie@hr.org')
+        company_contact = company.company_contacts.new(position: 'Hiring Manager', email: 'Julie@hr.org')
 
         expect(company_contact).to be_invalid
       end
 
       it 'is invalid if created without a position' do
         company = Company.create!(name: 'ESPN')
-        company_contact = company.contacts.create!(name: 'Julie Andrews', email: 'Julie@hr.org')
+        company_contact = company.company_contacts.new(name: 'Julie Andrews', email: 'Julie@hr.org')
 
         expect(company_contact).to be_invalid
       end
 
       it 'is invalid if created without an email' do
         company = Company.create!(name: 'ESPN')
-        company_contact = company.contacts.create!(name: 'Julie Andrews', position: 'Hiring Manager')
+        company_contact = company.company_contacts.new(name: 'Julie Andrews', position: 'Hiring Manager')
 
         expect(company_contact).to be_invalid
       end
