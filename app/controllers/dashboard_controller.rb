d4844f4b2c9cd259a34d7151cb0a0064c870f671
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-15 10:49:25 -0600

    Implementation of dashboard analysis top three companies by average interest level

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,5 @@
 class DashboardController < ApplicationController
-
   def index
-    
+    @top_three_companies = Job.top_three_companies_by_job_interest
   end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -4,7 +4,11 @@ class Job < ApplicationRecord
   belongs_to :company
   belongs_to :category
 
-  def sort_comments
-    Job.find(Job.id).job_comments.order(:created_at)
+  def self.sort_comments
+    find(Job.id).job_comments.order(:created_at)
+  end
+
+  def self.top_three_companies_by_job_interest
+    joins(:company).group(:name).order("average_level_of_interest DESC").average(:level_of_interest).take(3)
   end
 end
