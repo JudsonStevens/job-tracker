41bb8131d25e07a021dd1e39221fb21285b84887
Eliot Swank <eliotswank@gmail.com>
2018-05-15 11:44:46 -0600

    adds feature test for sort jobs by interest

---
+++ spec/features/jobs/user_applies_uri_with_query_params_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe "User enters the sort=interst query string in jobs URI" do
+  describe "and sees a list of jobs" do
+    it "sorted by level of interest" do
+      company_1 = Company.create(name: "Foo")
+      company_2 = Company.create(name: "Bar")
+      company_3 = Company.create(name: "Baz")
+      cat_1 = Category.create(title: "Dev")
+      cat_2 = Category.create(title: "QA")
+      job_1 = company_1.jobs.create(title: "Foobar", description: "qwerty", level_of_interest: 50, city: "lorem", category_id: cat_1.id)
+      job_2 = company_2.jobs.create(title: "Barbaz", description: "asdf", level_of_interest: 60, city: "ipsum", category_id: cat_1.id)
+      job_3 = company_3.jobs.create(title: "Bazfoo", description: "zxcvb", level_of_interest: 70, city: "loreminem", category_id: cat_1.id)
+      job_4 = company_2.jobs.create(title: "Foobaz", description: "uiop", level_of_interest: 80, city: "degoba", category_id: cat_2.id)
+      job_5 = company_3.jobs.create(title: "Bazbar", description: "zxcvbn", level_of_interest: 90, city: "Hoth", category_id: cat_2.id)
+      jobs = Job.all
+
+      visit '/jobs?sort=interest'
+
+      expect(page).to have_content(jobs)
+      expect(job_5).to appear_before(job_4)
+      expect(job_4).to appear_before(job_3)
+      expect(job_3).to appear_before(job_2)
+      expect(job_2).to appear_before(job_1)
+
+    end
+  end
+end
