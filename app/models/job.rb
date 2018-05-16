b358af717b6a084e5af582a3a13e78f116889507
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-15 20:24:54 -0600

    Implementation of jobs sorted by city on dashboard

--- app/models/job.rb
+++ app/models/job.rb
@@ -17,6 +17,6 @@ class Job < ApplicationRecord
   end
 
   def self.count_of_jobs_by_location
-    group(:city).order(count_id: :desc).count(:id)
+    group(:city).order('count_id DESC').count(:id)
   end
 end
