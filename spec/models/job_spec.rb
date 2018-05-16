e9860f2d881aafbb3f741b91c7dc0fea13e58ab1
Eliot Swank <eliotswank@gmail.com>
2018-05-15 19:54:05 -0600

    adds test for sort_by_level_of_interest

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -87,6 +87,25 @@ describe Job do
         expect(jobs.all.sort_by_interest.last).to eq(job_1) 
       end
     end
-    
+
+    describe "The count_by_level_of_interest method" do
+      it "returns a count of jobs by level of interest"
+        company_1 = Company.create(name: "Foo")
+        company_2 = Company.create(name: "Bar")
+        company_3 = Company.create(name: "Baz")
+        cat_1 = Category.create(title: "Dev")
+        cat_2 = Category.create(title: "QA")
+        job_1 = company_1.jobs.create(title: "Foobar", description: "qwerty", level_of_interest: 50, city: "lorem", category_id: cat_1.id)
+        job_2 = company_2.jobs.create(title: "Barbaz", description: "asdf", level_of_interest: 50, city: "ipsum", category_id: cat_1.id)
+        job_3 = company_3.jobs.create(title: "Bazfoo", description: "zxcvb", level_of_interest: 80, city: "loreminem", category_id: cat_1.id)
+        job_4 = company_2.jobs.create(title: "Foobaz", description: "uiop", level_of_interest: 80, city: "degoba", category_id: cat_2.id)
+        job_5 = company_3.jobs.create(title: "Bazbar", description: "zxcvbn", level_of_interest: 90, city: "Hoth", category_id: cat_2.id)
+        jobs = Job.all
+
+      expect(jobs.size).to eq(5)
+      expect(jobs.sort_by_interest(50)).to eq(2)
+      expect(jobs.sort_by_interest(80)).to eq(2)
+      expect(jobs.sort_by_interest(90)).to eq(1)
+    end
   end
 end
