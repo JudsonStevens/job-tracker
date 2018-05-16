2245bc86b7c3578f803bc4bc101e605bb1cf52a1
Eliot Swank <eliotswank@gmail.com>
2018-05-15 21:22:00 -0600

    completes count_by_level_of_interest implementation

--- spec/features/dashboard/user_can_see_all_dashboard_information_spec.rb
+++ spec/features/dashboard/user_can_see_all_dashboard_information_spec.rb
@@ -29,7 +29,7 @@ describe 'User' do
       company_3 = Company.create(name: 'Turing')
       company_4 = Company.create(name: 'G School')
       category = Category.create(title: 'Web')
-      job_1 = company.jobs.create(title: "Developer", level_of_interest: 10, city: "Denver", category_id: category.id)
+      job_1 = company.jobs.create(title: "Developer", level_of_interest: 1, city: "Denver", category_id: category.id)
       job_2 = company_2.jobs.create(title: "QA Analyst", level_of_interest: 50, city: "New York City", category_id: category.id)
       job_3 = company_2.jobs.create(title: "Web Developer", level_of_interest: 50, city: "Baton Rouge", category_id: category.id)
       job_4 = company_2.jobs.create(title: "Manager", level_of_interest: 30, city: "Denver", category_id: category.id)
@@ -37,12 +37,16 @@ describe 'User' do
       job_6 = company_3.jobs.create(title: "HR", level_of_interest: 80, city: "New York City", category_id: category.id)
 
       visit dashboard_path
-
-      expect(page).to have_content("Jobs by Interest")
-      expect(page).to have_content("10  1")
-      expect(page).to have_content("30  2")
-      expect(page).to have_content("50  2")
-      expect(page).to have_content("80  1")
+save_and_open_page
+      within("#interests") do
+        expect(page).to have_content("Jobs by Interest")
+      end
+      within("#interest-table") do
+        expect(page).to have_content("1: 1")
+        expect(page).to have_content("30: 2")
+        expect(page).to have_content("50: 2")
+        expect(page).to have_content("80: 1")
+      end
     end
   end
 end
